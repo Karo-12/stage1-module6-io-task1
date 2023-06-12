eed4fa67638e55a774ba123290994cfd88cd311e
Karolina <karolinaskawinska12@gmail.com>
2023-06-12 23:55:43 +0200

    add solved task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,26 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile;
+        try( BufferedReader br
+                     = new BufferedReader(new java.io.FileReader(file.getPath()))) {
+            String name = String.valueOf(br.readLine()).split(": ")[1];
+            String age = String.valueOf(br.readLine()).split(": ")[1];
+            String email = String.valueOf(br.readLine()).split(": ")[1];
+            String phoneNum = String.valueOf(br.readLine()).split(": ")[1];
+            profile = new Profile(name, Integer.parseInt(age), email, Long.parseLong(phoneNum));
+            return profile;
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+       return new Profile();
     }
 }
